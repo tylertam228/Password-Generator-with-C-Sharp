c6edb9d375fff41b58e431d9319ed4a981e2e5e4
tylertam228 <tyler888.tt@gmail.com>
2026-03-04 11:38:35 +0800

    chore: Create ASP.NET Core Razer Pages Project

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PasswordGenerator.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
